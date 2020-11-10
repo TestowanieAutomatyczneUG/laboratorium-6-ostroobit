06adbdaf934d537c1d12e82718fc55b8921494de
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-10 19:33:25 +0100

    Doctests

--- src/user.py
+++ src/user.py
@@ -3,6 +3,37 @@ class User:
     numbers = "0123456789"
 
     def valid_password(self, password):
+        """
+        >>> u = User()
+
+        >>> u.valid_password("abc")
+        False
+
+        >>> u.valid_password("Abc")
+        False
+
+        >>> u.valid_password("Abc1")
+        False
+
+        >>> u.valid_password("Abc1*")
+        False
+
+        >>> u.valid_password("Abc*")
+        False
+
+        >>> u.valid_password("Abcdebfiewbfi1")
+        False
+
+        >>> u.valid_password("hewbuhbewhbf1*")
+        False
+
+        >>> u.valid_password("njenvjewovoew*")
+        False
+
+        >>> u.valid_password("Abcvewjvewvwevev(1")
+        True
+
+        """
         letters = 0
         numbers = 0
         schars = 0
@@ -14,6 +45,7 @@ class User:
                 numbers += 1
             elif char in self.letters.upper():
                 upper += 1
+                letters += 1
             else:
                 schars += 1
         
@@ -26,3 +58,7 @@ class User:
         if upper < 1:
             return False
         return True
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
