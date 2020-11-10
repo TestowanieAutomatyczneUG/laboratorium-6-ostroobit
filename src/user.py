9f8a77c60db425cb97735af783775e45bb19c5a5
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-10 19:26:02 +0100

    Code 5

--- src/user.py
+++ src/user.py
@@ -6,11 +6,14 @@ class User:
         letters = 0
         numbers = 0
         schars = 0
+        upper = 0
         for char in password:
             if char in self.letters:
                 letters += 1
             elif char in self.numbers:
                 numbers += 1
+            elif char in self.letters.upper():
+                upper += 1
             else:
                 schars += 1
         
@@ -19,4 +22,6 @@ class User:
         if numbers < 1:
             return False
         if schars < 1:
+            return False
+        if upper < 1:
             return False
